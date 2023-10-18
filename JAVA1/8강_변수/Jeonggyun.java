64beac3ef878f31db0e1fbb1113acd71536fdf1d
JeongGyun Na <dotseven57@gmail.com>
2023-10-18 17:24:45 +0900

    code: 8강

---
+++ JAVA1/8강_변수/Jeonggyun.java
@@ -0,0 +1,37 @@
+
+public class Jeonggyun {
+
+	public static void main(String[] args) {
+		// Variable
+		int a = 1; // Number -> integer  정수
+		System.out.println(a);
+		
+		double b = 1.1; // real number -> 실수
+		System.out.println(b);
+		
+		String c = "Hello World"; // String -> 문자
+		System.out.println(c);
+		
+		// latter 변수 이
+		String name = "leezche";
+		System.out.println("Hello, "+name+" ... "+name+" ... egoing ... bye");
+
+		double VAT = 10.0;
+		System.out.println(VAT);
+		//double w = 1.1;
+		//double q = 1;
+		//double q2 = (double) 1;
+
+		System.out.println(b);
+
+		// int e = 1.1;
+		//double r = 1.1;
+		int e = (int) 1.1;
+		System.out.println(e);
+
+		// 1 to String 
+		String f = Integer.toString(1);
+		System.out.println(f.getClass());
+	}
+	
+}
