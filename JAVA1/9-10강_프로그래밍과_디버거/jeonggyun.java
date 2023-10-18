5676b89bae1bfd9bce2f55fed5a86749a15d7cae
JeongGyun Na <dotseven57@gmail.com>
2023-10-18 17:29:46 +0900

    code: 10강 프로그래밍과_디버거

---
+++ JAVA1/9-10강_프로그래밍과_디버거/jeonggyun.java
@@ -0,0 +1,14 @@
+
+public class jeonggyun {
+
+	public static void main(String[] args) {
+	
+		//프로그램의 의미 -> 이게 만약 1억개라면 
+		System.out.println(1);
+		System.out.println(2);
+		System.out.println(3);
+		
+		
+	}
+
+}
